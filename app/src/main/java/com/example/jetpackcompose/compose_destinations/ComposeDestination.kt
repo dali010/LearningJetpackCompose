e1f33fa2261effa1ad239d994658448329ed5843
DALI <haythem.ta@Mac-mini-de-TA.local>
2022-09-08 15:42:07 +0100

    compose destination

---
+++ app/src/main/java/com/example/jetpackcompose/compose_destinations/ComposeDestination.kt
@@ -0,0 +1,91 @@
+package com.example.jetpackcompose.compose_destinations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.example.jetpackcompose.compose_destinations.destinations.PostScreenDestination
+import com.example.jetpackcompose.compose_destinations.destinations.ProfileScreenDestination
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.LocalDateTime
+
+class ComposeDestination : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DestinationsNavHost(navGraph = NavGraphs.root)
+        }
+    }
+}
+
+@Destination(start = true)
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navigator.navigate(
+                ProfileScreenDestination(
+                    User(
+                        name = "Chris P. Bacon",
+                        id = "userid",
+                        created = LocalDateTime.now()
+                    )
+                )
+            )
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+    user: User
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: ${user.name}", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
+    }
+}
+
---
+++ app/src/main/java/com/example/jetpackcompose/compose_destinations/User.kt
@@ -0,0 +1,12 @@
+package com.example.jetpackcompose.compose_destinations
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDateTime
+
+@Parcelize
+data class User(
+    val name: String,
+    val id: String,
+    val created: LocalDateTime
+): Parcelable
